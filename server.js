efbe22328b4e71b9dce281770f74b195bae75780
paredfed <paredfed@gmail.com>
2018-12-19 17:56:37 -0600

    heroku deploy? v3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlin
 mongoose.connect(MONGODB_URI);
 const app = express();
 const db = mongoose.connection;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("client/build"));
 
